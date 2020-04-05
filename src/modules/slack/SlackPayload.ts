e01d459be8a5da01437422c506dedb8918ff75d5
gihachi <38201016+gihachi@users.noreply.github.com>
2020-04-06 00:29:18 +0900

    impl, Payload for Slack incoming webhook and factory of Payload

---
+++ src/modules/slack/SlackPayload.ts
@@ -0,0 +1,16 @@
+export interface SlackPayload {
+    attachments : Attachment[]
+}
+
+export interface Attachment {
+    fallback : string
+    pretext : string
+    color : string
+    fields : Field[]
+}
+
+export interface Field {
+    title : string
+    value : string
+    short : boolean
+}
---
+++ src/modules/slack/SlackPayloadFactory.ts
@@ -0,0 +1,45 @@
+import {SlackPayload,Field} from './SlackPayload'
+
+export const makeSlackPayload = (events : GoogleAppsScript.Calendar.CalendarEvent[]) : SlackPayload => {
+    const fields : Field[] = []
+    for(let event of events){
+        const eventField : Field = {
+            title : "Event",
+            value : event.getTitle(),
+            short : false
+        }
+        let timeStr : string
+        if(event.isAllDayEvent){
+            timeStr = "All day"
+        }else{
+            const startTime : GoogleAppsScript.Base.Date = event.getStartTime()
+            const endTime : GoogleAppsScript.Base.Date = event.getEndTime()
+            timeStr = startTime.getHours()+':'+startTime.getMinutes()+'-'+endTime.getHours()+':'+endTime.getMinutes()
+        }
+        const timeFiled : Field = {
+            title : "Time",
+            value : timeStr,
+            short : true
+        }
+        const detailField = {
+            title : "Detail",
+            value : event.getDescription(),
+            short : false
+        }
+
+        fields.push(eventField)
+        fields.push(timeFiled)
+        fields.push(detailField)
+    }
+
+    return {
+        attachments : [
+            {
+                fallback : '明日の予定だよ〜〜〜',
+                pretext : '明日の予定だよ〜〜〜',
+                color : '#D00000',
+                fields : fields
+            }
+        ]
+    }
+}
