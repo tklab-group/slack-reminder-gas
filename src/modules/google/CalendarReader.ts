fde3ddb653158df7053a76e96d6af9bb77a6cecb
gihachi <38201016+gihachi@users.noreply.github.com>
2020-04-18 16:42:42 +0900

    modify variable name

--- src/modules/google/CalendarReader.ts
+++ src/modules/google/CalendarReader.ts
@@ -8,6 +8,6 @@ export const getTomorrowEvents = (calendarId : string) : GoogleAppsScript.Calend
 export const getEventsWithin30Minutes = (calendarId : string) : GoogleAppsScript.Calendar.CalendarEvent[] => {
     const calendar : GoogleAppsScript.Calendar.Calendar = CalendarApp.getCalendarById(calendarId)
     const now : Date = new Date()
-    const tenMinutesAfter : Date = new Date(now.getTime() + (30 * 60 * 1000))
-    return calendar.getEvents(now, tenMinutesAfter)
+    const thirtyMinutesAfter : Date = new Date(now.getTime() + (30 * 60 * 1000))
+    return calendar.getEvents(now, thirtyMinutesAfter)
 }
