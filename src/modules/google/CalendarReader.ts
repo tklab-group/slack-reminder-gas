c48e9f291666ace22ac92ddd3e6e380e1af1ea16
gihachi <38201016+gihachi@users.noreply.github.com>
2020-04-24 00:45:33 +0900

    fix, remove events  which have already started

--- src/modules/google/CalendarReader.ts
+++ src/modules/google/CalendarReader.ts
@@ -9,5 +9,15 @@ export const getEventsWithin30Minutes = (calendarId : string) : GoogleAppsScript
     const calendar : GoogleAppsScript.Calendar.Calendar = CalendarApp.getCalendarById(calendarId)
     const now : Date = new Date()
     const thirtyMinutesAfter : Date = new Date(now.getTime() + (30 * 60 * 1000))
-    return calendar.getEvents(now, thirtyMinutesAfter)
+    const eventArr : GoogleAppsScript.Calendar.CalendarEvent[] =  calendar.getEvents(now, thirtyMinutesAfter)
+
+    const returnEventArr : GoogleAppsScript.Calendar.CalendarEvent[] = []
+
+    for(let event of eventArr){
+        if(now < event.getStartTime() && event.getStartTime() < thirtyMinutesAfter){
+            returnEventArr.push(event)
+        }
+    }
+
+    return returnEventArr
 }
